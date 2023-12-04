f98f3ad2b0bd3c892bf6e10f2e3be5da924e769b
kypark <0422.kypark@gmail.com>
2023-12-05 00:36:29 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/b2k/memo/memo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.b2k.memo.memo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/ui/todo/todo_screen.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TodoScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
